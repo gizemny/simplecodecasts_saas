fe7a81b6aa7a7ac5110129caacceed6e8131c895
Gizem <gizemny@gmail.com>
2015-08-08 16:41:54 +0000

    added validations and create action for form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,6 +3,21 @@ class ContactsController < ApplicationController
         @contact = Contact.new
     end
     
+    #add variable contact params // flash :key comes from bootstrap classes
     def create
+        @contact = Contact.new(contact_params)
+         if @contact.save
+             flash[:success] = "Message sent."
+            redirect_to new_contact_path
+         else
+             flash[:danger] = "Error occured."
+             redirect_to new_contact_path
+         end     
     end
+    
+    #whitelist for security purpose
+    private
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
+#add form validation from the website
 class Contact < ActiveRecord::Base
+    validates :name, presence: true
+    validates :email, presence: true
 end
 
